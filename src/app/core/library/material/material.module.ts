b664cdeec7a203fad471cc3089cdcc9766bc704a
Дмитрий Маркавец <markavets.do@gazgolder.ru>
2021-06-11 19:14:01 +0300

    Add new logic

--- src/app/core/library/material/material.module.ts
+++ src/app/core/library/material/material.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 const MaterialComponents = [
-]
+  MatButtonModule,
+  MatToolbarModule,
+  MatSidenavModule,
+  MatListModule,
+  MatIconModule
+];
 
 @NgModule({
-  //imports: [MaterialComponents],
-  //exports: [MaterialComponents],
+  imports: [MaterialComponents],
+  exports: [MaterialComponents],
 })
 export class MaterialModule { }
--- src/app/modules/main/components/components.module.ts
+++ src/app/modules/main/components/components.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { LibraryModule } from 'src/app/core/library/library.module';
 import { FooterComponent } from './footer/footer.component';
 import { NavbarComponent } from './navbar/navbar.component';
 
@@ -9,7 +10,8 @@ import { NavbarComponent } from './navbar/navbar.component';
     FooterComponent
   ],
   imports: [
-    CommonModule
+    RouterModule,
+    LibraryModule
   ],
   exports: [
     NavbarComponent,
